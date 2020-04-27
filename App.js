09aeffc9645554019523bdf65b1a8f9486fe2982
martycabj1999 <martingira1999@gmail.com>
2020-04-27 00:04:26 -0300

    comienzo

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
